acaa32031a364c539938db078c6595573ba5243d
Michael Franzen <franzen@arch.ethz.ch>
2016-08-29 19:25:15 +0200

    Prepared loaders.

---
+++ include/qua-vis/loader.h
@@ -0,0 +1,8 @@
+#ifndef QUA_VIS_LOADER_H
+#define QUA_VIS_LOADER_H
+
+namespace quavis {
+
+}
+
+#endif
---
+++ include/qua-vis/loaders/file-loader.h
@@ -0,0 +1,10 @@
+#ifndef QUA_VIS_FILE_LOADER_H
+#define QUA_VIS_FILE_LOADER_H
+
+namespace quavis {
+  namespace loaders {
+
+  }
+}
+
+#endif
