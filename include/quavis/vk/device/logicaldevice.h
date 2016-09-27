19ccd539e55656d4e3e9c08cf8c8b31bd4328f97
Michael Franzen <franzen@arch.ethz.ch>
2016-09-27 12:57:02 +0200

    Drafted new classes for logical and physical device.

--- include/quavis/vk/device/logicaldevice.h
+++ include/quavis/vk/device/logicaldevice.h
@@ -4,65 +4,63 @@
 #include <vector>
 
 namespace quavis {
-  namespace logicaldevice {
-    class LogicalDevice {
-    public:
-      /**
-      * Creates a new logical device on the selected physical device.
-      * Optionally, the number of graphics- and compute queues can be
-      * specfified.
-      *
-      * The method selects 2 (not necessarily different) queue families
-      * from the specified device.
-      * The graphics queues are guaranteed to be usable for graphics while
-      * the compute queues are usable for compute shaders and
-      * the transfer queues are usable for memory transfer.
-      */
-      LogicalDevice(VkPhysicalDevice physical_device, uint32_t graphics_queues = 1, uint32_t compute_queues = 1, uint32_t transfer_queues = 1);
+  class LogicalDevice {
+  public:
+    /**
+    * Creates a new logical device on the selected physical device.
+    * Optionally, the number of graphics- and compute queues can be
+    * specfified.
+    *
+    * The method selects 2 (not necessarily different) queue families
+    * from the specified device.
+    * The graphics queues are guaranteed to be usable for graphics while
+    * the compute queues are usable for compute shaders and
+    * the transfer queues are usable for memory transfer.
+    */
+    LogicalDevice(VkPhysicalDevice physical_device, uint32_t graphics_queues = 1, uint32_t compute_queues = 1, uint32_t transfer_queues = 1);
 
-      /**
-      * Destroys the logical device safely. Note that all objects that are
-      * dependant on the logical device need to be destroyed beforehand.
-      */
-      ~LogicalDevice();
+    /**
+    * Destroys the logical device safely. Note that all objects that are
+    * dependant on the logical device need to be destroyed beforehand.
+    */
+    ~LogicalDevice();
 
-      /**
-      * The vulkan handler to the logical device.
-      */
-      VkDevice device;
+    /**
+    * The vulkan handler to the logical device.
+    */
+    VkDevice vk_handle;
 
-      /**
-      * The graphics queues
-      */
-      std::vector<VkQueue> graphics_queues();
+    /**
+    * The graphics queues (used for render pass)
+    */
+    std::vector<VkQueue> graphics_queues();
 
-      /**
-      * The compute queues
-      */
-      std::vector<VkQueue> compute_queues();
+    /**
+    * The compute queues (used for compute shader)
+    */
+    std::vector<VkQueue> compute_queues();
 
-      /**
-      * The transfer queues
-      */
-      std::vector<VkQueue> compute_queues();
+    /**
+    * The transfer queues (used for memory transfer)
+    */
+    std::vector<VkQueue> compute_queues();
 
-    private:
-      uint32_t SelectQueueFamily(VkQueueFlags required_flags = VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT | VK_QUEUE_TRANSFER_BIT);
-      uint32_t RetrieveQueue(uint32_t queue_family_index, uint32_t index);
+  private:
+    uint32_t GetQueueFamily(VkQueueFlags required_flags = VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT | VK_QUEUE_TRANSFER_BIT);
+    uint32_t GetQueue(uint32_t queue_family_index, uint32_t queue_index);
 
-      // physical device (set in constructor)
-      VkPhysicalDevice vk_physical_device_;
+    // physical device (set in constructor)
+    VkPhysicalDevice vk_physical_device_;
 
-      // Default features used for a logical device
-      const VkPhysicalDeviceFeatures vk_features_;
-      vk_features.tessellationShader = VK_TRUE;
-      vk_features.geometryShader = VK_TRUE;
-      vk_features.fillModeNonSolid = VK_TRUE;
+    // Default features used for a logical device
+    VkPhysicalDeviceFeatures vk_features_;
+    vk_features.tessellationShader = VK_TRUE;
+    vk_features.geometryShader = VK_TRUE;
+    vk_features.fillModeNonSolid = VK_TRUE;
 
-      // Default extensions
-      const std::vector<std::string> vk_logical_device_extensions_();
-    };
-  }
+    // Default extensions
+    const std::vector<const char*> extensions_();
+  };
 }
 
 #endif
--- include/quavis/vk/device/physicaldevice.h
+++ include/quavis/vk/device/physicaldevice.h
@@ -1,6 +1,42 @@
 #ifndef QUAVIS_PHYSICALDEVICE_H
 #define QUAVIS_PHYSICALDEVICE_H
 
+class PhysicalDevice {
+public:
+  /**
+  * Finds and picks an appropriate physical device that supports the
+  * specified queue types, extensions and layers.
+  */
+  PhysicalDevice(VkInstance instance);
 
+  /*
+  * The vulkan object of the physical device
+  */
+  VkPhysicalDevice vk_handle;
+
+  /**
+  * Checks for a physical device whether it supports a given extension
+  */
+  static bool hasExtension(VkPhysicalDevice device, const char* extension_name);
+
+  /**
+  * Checks for a physical device whether it supports a given layer
+  */
+  static bool hasLayer(VkPhysicalDevice device, const char* layer_name);
+
+  /**
+  * Checks for a physical device whether it supports a given queue type
+  */
+  static bool hasQueueType(VkPhysicalDevice device, VkQueueFlags queue_flags);
+
+private:
+  const std::vector<const char*> extensions_ = {
+    "VK_EXT_debug_report"
+  };
+
+  const std::vector<const char*> layers_ = {
+    "VK_LAYER_LUNARG_standard_validation"
+  };
+};
 
 #endif
